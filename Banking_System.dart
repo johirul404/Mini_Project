2e9efe57c2d4c0ad5e2338e24deb3fcb17e1f034
johirul404 <johirulislam@iut-dhaka.edu>
2026-03-28 12:03:09 +0600

    Initial commit

---
+++ Banking_System.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+abstract class BankAccount{
+  BankAccount({required this.balance});
+  double balance;
+  void deposit(double amount){
+    balance += amount;
+  }
+  void withdraw(double amount);
+}
+class SavingsAccount extends BankAccount{
+  SavingsAccount(double balance) : super(balance: balance);
+  void withdraw(double amount){
+    if(balance >= amount){
+      balance -= amount;
+    }else{
+      print("Insufficient funds");
+    }
+  }
+}
+class CheckingAccount extends BankAccount{
+  CheckingAccount(double balance) : super(balance : balance);
+  void withdraw(double amount){
+    if(balance >= amount){
+      balance -= amount;
+    } else{
+      balance -= amount;
+      if(balance < 0){
+        print("Overdraft fee applied");
+        balance -= 10;
+      }
+    }
+  }
+}
+main(){
+
+}
